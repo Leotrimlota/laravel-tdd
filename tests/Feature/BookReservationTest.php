19da06cead98b005123b296c47b1a41719c0df82
Leotrim <leotrim@sprigs.eu>
2022-08-28 02:08:04 +0200

    replace prefix methods name test_ with annotation

--- tests/Feature/BookReservationTest.php
+++ tests/Feature/BookReservationTest.php
@@ -2,21 +2,23 @@
 
 namespace Tests\Feature;
 
- use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Book;
 use Tests\TestCase;
 
 class BookReservationTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_a_book_can_be_added_to_the_library()
+
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
     {
-        $this->withoutExceptionHandling();
         $response = $this->post('/books', [
                 "title" => "Cool Book",
                 "author" => "Victor"
@@ -24,10 +26,11 @@ class BookReservationTest extends TestCase
         );
 
         $response->assertOk();
-        $this->assertCount(1,Book::all());
+        $this->assertCount(1, Book::all());
     }
 
-    public function test_a_title_is_required()
+    /** @test */
+    public function a_title_is_required()
     {
         $response = $this->post('/books', [
                 "title" => '',
@@ -38,33 +41,35 @@ class BookReservationTest extends TestCase
         $response->assertSessionHasErrors('title');
     }
 
-    public function test_a_author_is_required()
+    /** @test */
+    public function a_author_is_required()
     {
         $response = $this->post('/books', [
                 "title" => 'Cool Book',
-                "author" => ""
+                "author" => ''
             ]
         );
 
         $response->assertSessionHasErrors('author');
     }
 
-    public function test_a_book_can_be_updated()
+    /** @test */
+    public function a_book_can_be_updated()
     {
-        $this->withoutExceptionHandling();
         $this->post('/books', [
-                "title" => 'Cool Book',
-                "author" => "Victor"
+                'title' => 'Cool Book',
+                'author' => 'Victor'
             ]
         );
 
         $book = Book::first();
-        $response = $this->patch("/books/{$book->id}",[
-            'title'=>'New Title',
-            'author'=>'New Author'
+        $this->patch("/books/{$book->id}", [
+            'title' => 'New Title',
+            'author' => 'New Author'
         ]);
 
-        $this->assertEquals("New Title",Book::first()->title);
-        $this->assertEquals("New Author",Book::first()->author);
+        $this->assertEquals("New Title", Book::first()->title);
+        $this->assertEquals("New Author", Book::first()->author);
     }
+
 }
